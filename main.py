d60710f97129c889abe63a18e5fd90a46b0ab0a1
Alexandre <fd.khelf@gmail.com>
2023-10-18 16:13:38 +0200

    test ci github actions

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from src.calculateArea import areaSquare
+
+def main():
+    """_summary_
+    """
+    res = areaSquare(length= "3.3")
+    print(res)
+
 if __name__ == "__main__":
-    print("Hello World!")
+    main()
